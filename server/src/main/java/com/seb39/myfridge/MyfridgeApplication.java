4e350430690a4a788600cbd7071c202e06f73de6
sj <slaayer60@gmail.com>
2022-09-14 10:58:32 +0900

    spring init

---
+++ server/src/main/java/com/seb39/myfridge/MyfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.seb39.myfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfridgeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb39/myfridge/MyfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb39.myfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
