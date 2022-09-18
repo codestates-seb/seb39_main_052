abbf0ef205e3372859d2131bdf1fe9603148bb5d
hana1203 <hanaindec@gmail.com>
2022-09-18 11:50:30 +0900

    design: 로그인 컴포넌트 rough css

---
+++ client/src/components/layout/LogInStyle.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const LogInContainer = styled.section`
+  display: flex;
+  justify-content: center;
+
+  > form {
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    padding: 10px;
+    /* background-color: turquoise; //just for checking */
+  }
+
+  > form > label {
+    margin-top: 10px;
+  }
+
+  > form > input {
+    border: none;
+    border-bottom: 2px solid var(--fridge-500);
+    width: 300px;
+    margin: 10px 0;
+  }
+
+  > form > span {
+    font-size: 14px;
+    color: var(--gray-600);
+  }
+`;
