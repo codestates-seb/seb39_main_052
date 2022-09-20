607d61a8e65b45697d425b9056228be6d20d2c49
hana1203 <hanaindec@gmail.com>
2022-09-21 00:40:37 +0900

    feat: OAuth2 로그인요청시 리다이렉트 페이지에서 액세스토큰 받아오기

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import GlobalStyle from './GlobalStyle';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Nav from './components/layout/Nav/Nav';
-import LogInForm from "./components/layout/RegisterForm/LogInForm";
+import Nav from "./components/layout/Nav/Nav";
 import SignUpForm from "./components/layout/RegisterForm/SignUpForm";
-import NewRecipe from './pages/NewRecipe/NewRecipe';
+import NewRecipe from "./pages/NewRecipe/NewRecipe";
 import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
+import OAuth2RedirectHandler from "./components/layout/RegisterForm/OAuth2RedirectHandler";
 
 function App() {
   return (
@@ -16,13 +16,13 @@ function App() {
       {/* Global Styles는 Router 안, 컴포넌트 상단에 위치  */}
       <GlobalStyle />
       <BrowserRouter>
-      <Nav />
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/recipes/new" element={<NewRecipe />} />
-        <RecipeEditor />
+          <Route path="/auth/redirect" element={<OAuth2RedirectHandler />} />
         </Routes>
       </BrowserRouter>
     </>
