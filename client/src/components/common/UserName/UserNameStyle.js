1e3dc6ee0f14d6d6e707e2d1442fc927a19ec069
imsujinpark <imsujinpark@gmail.com>
2022-09-21 01:09:03 +0900

    feat: 유저 아이콘 + 이름 컴포넌트 생성 (검색 페이지, 상세페이지, 댓글 등에 활용)

---
+++ client/src/components/common/UserName/UserNameStyle.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 0 0 0 0;
+    color: var(--fridge-800);
+`
+
+export const Image = styled.img`
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    margin-bottom: 1px;
+`
+
+export const Name = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 0 0 8px;
+`
