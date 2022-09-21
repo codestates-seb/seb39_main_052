45410a4e8b58f1ba4860f6769d89324b113a3b9d
imsujinpark <imsujinpark@gmail.com>
2022-09-21 11:55:48 +0900

    feat: 클릭 시 확인 여부를 체크하는 useConfirm 외부 함수 생성

--- client/src/components/layout/Comments/Comments.js
+++ client/src/components/layout/Comments/Comments.js
@@ -3,6 +3,7 @@ import { Comment, CommentRow, Container, Input, InputWrapper } from "./CommentsS
 import blankImage from "../../../assets/blankImage.webp";
 import UserName from "../../common/UserName/UserName";
 import GeneralButton from "../../common/Button/GeneralButton";
+import useConfirm from "../../../hooks/useConfirm";
 import { useState } from "react";
 
 const Comments = () => {
@@ -13,35 +14,47 @@ const Comments = () => {
         id: 1,
         username: `바닐라봉봉`,
         profile: blankImage,
-        content: `맛있어보여요~`,
+        content: `맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛`,
     }
 
     const handleChange = (e) => {
         setComment(e.target.value);
     }
 
-    const handleSubmit = () => {
-        // axios({
-        //     method: `post`,
-        //     url: `/api/recipes/${dummy.id}/comments`,
-        //     headers: {
-        //     },
-        //     data: comment,
-        // })
-        // .then((response) => {
-        //     console.log(response)
-        // })
-        // .catch((error) => {
-        //     // 예외 처리
-        //     console.log(error.response);
-        // })
-    }
+    console.log(`hi`)
+
+    const confirm = () => console.log("Deleting the world...");
+    const cancel = () => console.log("Aborted");
+    const handleSubmit = useConfirm("정말 삭제하시겠습니까?", confirm, cancel);
+
+    // const handleSubmit = () => {
+    //     confirmation();
+    //     // axios({
+    //     //     method: `post`,
+    //     //     url: `/api/recipes/${dummy.id}/comments`,
+    //     //     headers: {
+    //     //     },
+    //     //     data: comment,
+    //     // })
+    //     // .then((response) => {
+    //     //     console.log(response)
+    //     // })
+    //     // .catch((error) => {
+    //     //     // 예외 처리
+    //     //     console.log(error.response);
+    //     // })
+    // }
 
     return (
         <Container>
             <h2>댓글</h2>
             <InputWrapper>
-                <Input placeholder="댓글을 입력하세요" onChange={handleChange}></Input>
+                <Input
+                    placeholder="댓글을 입력하세요"
+                    onChange={handleChange}
+                    type='text'
+                    maxLength='28'
+                />
                 <GeneralButton className={`small`} onClick={handleSubmit}>등록</GeneralButton>
             </InputWrapper>
             <CommentRow>
