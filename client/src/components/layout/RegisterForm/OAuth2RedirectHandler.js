3170a86297f7debb190c25088e60080ca59081f2
hana1203 <hanaindec@gmail.com>
2022-09-21 10:18:31 +0900

    feat: oAuth redirect 페이지에서 useEffect로 navigate

--- client/src/components/layout/RegisterForm/OAuth2RedirectHandler.js
+++ client/src/components/layout/RegisterForm/OAuth2RedirectHandler.js
@@ -8,17 +8,24 @@ import LogIn from "../../../pages/LogIn/LogIn";
 const OAuth2RedirectHandler = () => {
   //   const [isSocialLoggedIn, setisSocialLoggedIn] = useState(false);
   const navigate = useNavigate();
-  //   useEffect(() => {
-  //   try {
-  //구글 로그인해서 액세스 토큰 받아오면 액세스 토큰 로컬스토리지에 저장후 홈페이지로 가기
+
   let location = new URL(window.location.href);
   console.log("오오쓰 리다이렉트 핸들러 location", location);
   const ACCESS_TOKEN_FOR_OAUTH = location.searchParams.get("access-token");
   console.log("ACCESS_TOKEN_FOR_OAUTH", ACCESS_TOKEN_FOR_OAUTH); //eyJ0eXAiOiJKV1QiLC...
+
+  useEffect(() => {
+    //   try {
+    //구글 로그인해서 액세스 토큰 받아오면 액세스 토큰 로컬스토리지에 저장후 홈페이지로 가기
+    navigate("/");
+    console.log("유즈이펙트");
+  }, [ACCESS_TOKEN_FOR_OAUTH]); //navigate쓸때 useEffect 에서 쓰라고해서
+
   // if (ACCESS_TOKEN_FOR_OAUTH) {
   localStorage.setItem("ACCESS_TOKEN_FOR_OAUTH", ACCESS_TOKEN_FOR_OAUTH);
-
-  navigate(<Home></Home>);
+  console.log("로컬스토리지 이후에", ACCESS_TOKEN_FOR_OAUTH);
+  // navigate(<Home></Home>);
+  // history.push
 
   //   <Link to="/"></Link>;
   //   setisSocialLoggedIn(true);
@@ -26,7 +33,7 @@ const OAuth2RedirectHandler = () => {
   //   } catch (error) {
   //     console.log(error, "OAuth 액세스토큰 받기 에러");
   //   } //try catch로 잘못넣으면 무한 요청;;
-  //   }, [navigate]); //navigate쓸때 useEffect 에서 쓰라고해서
+
   //   isSocialLoggedIn ? <Link to="/"></Link> : <Link to="/login"></Link>;
   return <div>스피너 넣을것</div>;
 };
