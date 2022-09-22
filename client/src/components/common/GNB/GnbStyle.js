d09c1ef616dcb3cd28597851a41fe37377183f87
hana1203 <hanaindec@gmail.com>
2022-09-22 13:42:20 +0900

    feat: GNB 대략적인 구조 마크업, 기본 로고 assets 폴더에 저장

---
+++ client/src/components/common/GNB/GnbStyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+`;
+export const Nav = styled.nav`
+  display: flex;
+`;
+export const LeftBox = styled.div``;
+export const LogoBox = styled.div``;
+export const Img = styled.img`
+  width: 118px;
+  height: 68px;
+`;
+export const RightBox = styled.div``;
