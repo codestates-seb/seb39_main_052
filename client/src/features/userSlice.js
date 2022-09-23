4e1427fa91b9712c3fcdc5e0a3560425a791889e
hana1203 <hanaindec@gmail.com>
2022-09-23 15:44:58 +0900

    feat: redux 슬라이스에 유저 정보 저장하는 setUserInfo 리듀서 추가

--- client/src/features/userSlice.js
+++ client/src/features/userSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isLoggedIn: false,
   userId: null,
-  userEmail: null,
+  userName: null,
+  userProfileImgPath: null,
 };
 
 const userSlice = createSlice({
@@ -11,19 +12,27 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     //로그인 성공시
-    setLoginSuccess(state, action) {
+    setLoggedIn(state, action) {
       state.isLoggedIn = true;
+    },
+
+    //유저정보 저장
+    setUserInfo(state, action) {
       state.userId = action.payload.userId;
+      state.userName = action.payload.userName;
+      state.userProfileImgPath = action.payload.userProfileImgPath;
       //로그인 성공하면 이미지링크도 슬라이스에 저장하기 추가
     },
 
-    //로그아웃시 상태 초기화
+    //로그아웃시 유저 로그인 상태 false, 저장된 정보 초기화
     setLoggedOut(state) {
       state.isLoggedIn = false;
       state.userId = null;
+      state.userName = null;
+      state.userProfileImgPath = null;
     },
   },
 });
 
 export default userSlice;
-export const { setLoginSuccess, setLoggedOut } = userSlice.actions;
+export const { setLoggedIn, setUserInfo, setLoggedOut } = userSlice.actions;
