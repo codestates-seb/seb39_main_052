9d1cbd5d1a8f77fa8319a133e62ac2394d1026f7
imsujinpark <imsujinpark@gmail.com>
2022-09-23 17:18:15 +0900

    feat: 모달창에 사용될 search 바 컴포넌트 생성

---
+++ client/src/components/layout/ModalSearchBar.js/ModalSearchBarStyle.js
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+
+export const SearchBar = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 68%;
+    height: 48px;
+    border: 2px solid var(--fridge-300);
+    background-color: var(--gray-050);
+    padding: 4px 4px 4px 16px;
+    border-radius: 8px;
+`
+export const SearchInput = styled.input`
+    flex-grow: 1;
+    border: none;
+    font-size: 16px;
+    margin: 2px 8px 0 16px;
+`
+export const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+    color: var(--fridge-800);
+    font-size: 18px;
+`
+
+
