00f3e27fd2c99c51d74c0fdd5e919b1469af9cee
sj <slaayer60@gmail.com>
2022-09-23 15:35:08 +0900

    feat: 레시피 하트 기능 추가

---
+++ server/src/main/java/com/seb39/myfridge/heart/service/HeartService.java
@@ -0,0 +1,60 @@
+package com.seb39.myfridge.heart.service;
+
+import com.seb39.myfridge.heart.entity.Heart;
+import com.seb39.myfridge.heart.repository.HeartRepository;
+import com.seb39.myfridge.member.entity.Member;
+import com.seb39.myfridge.member.service.MemberService;
+import com.seb39.myfridge.recipe.entity.Recipe;
+import com.seb39.myfridge.recipe.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class HeartService {
+
+    private final MemberService memberService;
+    private final RecipeService recipeService;
+    private final HeartRepository heartRepository;
+
+    @Transactional
+    public void addHeartToRecipe(Long memberId, Long recipeId){
+        verifyAddHeartPossible(memberId,recipeId);
+        addHeart(memberId,recipeId);
+    }
+
+    @Transactional
+    public void removeHeartToRecipe(Long memberId, Long recipeId){
+        Heart heart = findHeart(memberId, recipeId);
+        removeHeart(heart);
+    }
+
+
+    public int findHeartCounts(Long recipeId){
+        return heartRepository.countByRecipeId(recipeId);
+    }
+
+    private Heart findHeart(Long memberId, Long recipeId){
+        return heartRepository.findByMemberIdAndRecipeId(memberId, recipeId)
+                .orElseThrow(()-> new IllegalArgumentException("heart not exist. member = " + memberId + " recipe = " + recipeId));
+    }
+
+    private void verifyAddHeartPossible(Long memberId, Long recipeId) {
+        if(heartRepository.existsByMemberIdAndRecipeId(memberId,recipeId))
+            throw new IllegalArgumentException("heart is already marked");
+    }
+
+    private void addHeart(Long memberId, Long recipeId){
+        Member member = memberService.findById(memberId);
+        Recipe recipe = recipeService.findRecipeById(recipeId);
+        heartRepository.save(new Heart(member, recipe));
+    }
+
+    private void removeHeart(Heart heart){
+        heartRepository.delete(heart);
+    }
+
+
+}
---
+++ server/src/test/java/com/seb39/myfridge/heart/repository/HeartRepositoryTest.java
@@ -0,0 +1,132 @@
+package com.seb39.myfridge.heart.repository;
+
+import com.seb39.myfridge.heart.entity.Heart;
+import com.seb39.myfridge.member.entity.Member;
+import com.seb39.myfridge.member.repository.MemberRepository;
+import com.seb39.myfridge.recipe.entity.Recipe;
+import com.seb39.myfridge.recipe.repository.RecipeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest
+@Transactional
+class HeartRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    RecipeRepository recipeRepository;
+    @Autowired
+    HeartRepository heartRepository;
+
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+    @DisplayName("heart가 있을 때 existsByMemberIdAndRecipeId가 true를 반환한다.")
+    void existHeart() throws Exception {
+        // given
+        Member member = Member.generalBuilder()
+                .name("SJ")
+                .buildGeneralMember();
+        memberRepository.save(member);
+
+        Recipe recipe = new Recipe();
+        recipe.setTitle("Test recipe");
+        recipe.setMember(member);
+        recipeRepository.save(recipe);
+
+        Heart heart = new Heart(member, recipe);
+        heartRepository.save(heart);
+
+        // when
+        boolean result = heartRepository.existsByMemberIdAndRecipeId(member.getId(), recipe.getId());
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("heart가 없을 때 existsByMemberIdAndRecipeId가 false  반환한다.")
+    void notExistHeart() throws Exception {
+        // given
+        Member member = Member.generalBuilder()
+                .name("SJ")
+                .buildGeneralMember();
+        memberRepository.save(member);
+
+        Recipe recipe = new Recipe();
+        recipe.setTitle("Test recipe");
+        recipe.setMember(member);
+        recipeRepository.save(recipe);
+
+        // when
+        boolean result = heartRepository.existsByMemberIdAndRecipeId(member.getId(), recipe.getId());
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("특정 레시피의 heart 개수를 반환한다.")
+    void countHeartsByRecipe() throws Exception {
+        // given
+        Member member = Member.generalBuilder()
+                .name("SJ")
+                .buildGeneralMember();
+        memberRepository.save(member);
+
+        Recipe recipe = new Recipe();
+        recipe.setTitle("Test recipe");
+        recipe.setMember(member);
+        recipeRepository.save(recipe);
+
+        int heartCount = 17;
+        for(int i=0;i<heartCount;i++){
+            heartRepository.save(new Heart(null,recipe));
+        }
+
+        // when
+        int result = heartRepository.countByRecipeId(recipe.getId());
+
+        // then
+        assertThat(result).isEqualTo(heartCount);
+    }
+
+    @Test
+    @DisplayName("findByMemberIdAndRecipeId test")
+    void findHeart() throws Exception {
+        // given
+        Member member = Member.generalBuilder()
+                .name("SJ")
+                .buildGeneralMember();
+        memberRepository.save(member);
+
+        Recipe recipe = new Recipe();
+        recipe.setTitle("Test recipe");
+        recipe.setMember(member);
+        recipeRepository.save(recipe);
+
+        Heart heart = new Heart(member, recipe);
+        heartRepository.save(heart);
+
+        // when
+        Optional<Heart> optionalHeart = heartRepository.findByMemberIdAndRecipeId(member.getId(), recipe.getId());
+
+        // then
+        assertThat(optionalHeart.isPresent()).isTrue();
+    }
+}
