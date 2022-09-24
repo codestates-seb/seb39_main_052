676f7f04d8dbd58d4b95a1130f4d22f972fe82f6
hana1203 <hanaindec@gmail.com>
2022-09-24 23:20:43 +0900

    design: GNB 미디어 쿼리 적용

--- client/src/components/common/GNB/Gnb.js
+++ client/src/components/common/GNB/Gnb.js
@@ -5,6 +5,7 @@ import {
   Header,
   Nav,
   LeftBox,
+  LeftBoxForMobile,
   LogoBox,
   Img,
   RightBox,
@@ -15,18 +16,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faUserLarge,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import DropDownMenu from "../../layout/DropDown/DropDownMenu";
 
 //로그인 상태 가져오려고 리덕스 저장소 정보 import
 import { useSelector, useDispatch } from "react-redux";
-import { setLoginSuccess, setLoggedOut } from "../../../features/userSlice";
 
 import { useState } from "react";
 import RecipeSearchModal from "../../layout/Modal/RecipeSearchModal";
 
 const Gnb = () => {
-  // //로그인 상태 가져와서 변수에 저장
+  //로그인 상태 가져와서 변수에 저장
   const isLoggedIn = useSelector((state) => {
     return state.user.isLoggedIn;
   });
@@ -40,20 +41,33 @@ const Gnb = () => {
   //search bar 모달 상태
   const [showModal, setShowModal] = useState(false);
 
+  //모바일 화면에서 보이는 왼쪽 상단 햄버거 메뉴 토글 상태
+  const [isToggleOpen, setIsToggleOpen] = useState(false);
+
   const clickModal = () => {
     setShowModal(!showModal);
   };
   // console.log("showModal 상태?", showModal);
 
+  const handleToggle = () => {
+    setIsToggleOpen(!isToggleOpen);
+  };
+  console.log("isToggleOpen상태", isToggleOpen);
+
   return (
     <Header>
       <Nav>
-        <LeftBox className="leftbox">
-          <Ul>
-            <Li>냉장고 파먹기</Li>
-            <Li>나의 냉장고</Li>
+        <LeftBox className="leftbox" isToggleOpen={isToggleOpen}>
+          <Ul className="nav-menu-list">
+            <Li className="each-nav-menu-list">냉장고 파먹기</Li>
+            <Li className="each-nav-menu-list">나의 냉장고</Li>
           </Ul>
         </LeftBox>
+        {/* for Mobile toggle */}
+        <LeftBoxForMobile className="menuToggle" onClick={handleToggle}>
+          <FontAwesomeIcon icon={faBars} size="lg" />{" "}
+        </LeftBoxForMobile>
+
         <LogoBox className="logo">
           <Link to="/">
             <Img src={largelogoblack} />
--- client/src/components/common/GNB/GnbStyle.js
+++ client/src/components/common/GNB/GnbStyle.js
@@ -13,7 +13,8 @@ export const Header = styled.header`
 
   /* media query 예시. theme.js에서 정의한 theme을 props로 받아서 mobile 사이즈 가져온다 */
   @media ${({ theme }) => theme.device.mobile} {
-    color: pink;
+    /* color: pink; */
+    max-height: 88px;
   }
 `;
 export const Nav = styled.nav`
@@ -21,7 +22,39 @@ export const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
 `;
-export const LeftBox = styled.div``;
+
+//냉장고 파먹기, 나의 냉장고 담고있는 박스 . 미디어 쿼리시 navBar아래에 위치
+export const LeftBox = styled.div`
+  @media ${({ theme }) => theme.device.mobile} {
+    display: ${(props) => (props.isToggleOpen ? "block" : "none")};
+    width: 100%;
+    /* height: 40px; */
+    position: absolute;
+    top: 88px;
+    left: 0;
+    margin: 0;
+    background-color: var(--fridge-300);
+  }
+`;
+
+//메뉴 토글 담는 박스 - LeftBoxForMobile로 컴포넌트화 했기떄문에 class 붙이지 않아도 작동!
+export const LeftBoxForMobile = styled.div`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  &.menuToggle {
+    display: none; /* 모바일 화면 아닐떄는 안보임 */
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    &.menuToggle {
+      display: block; /*모바일 화면에서는 메뉴 토글 보여주기*/
+      :hover {
+        color: white;
+      }
+    }
+  }
+`;
+
 export const LogoBox = styled.div`
   display: flex;
 `;
@@ -34,9 +67,24 @@ export const Img = styled.img`
 
 export const Ul = styled.ul`
   display: flex;
+
+  @media ${({ theme }) => theme.device.mobile} {
+    &.nav-menu-list {
+      justify-content: space-around;
+    }
+  }
 `;
 
 export const Li = styled.li`
   cursor: pointer;
   padding: 0 32px;
+
+  @media ${({ theme }) => theme.device.mobile} {
+    &.each-nav-menu-list {
+      :hover {
+        border-radius: 10px;
+        background-color: var(--fridge-500);
+      }
+    }
+  }
 `;
