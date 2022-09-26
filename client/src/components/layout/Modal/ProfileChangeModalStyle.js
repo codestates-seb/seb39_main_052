bec7ce2b40480e0d7da8c16af5f1f4b795a271da
hana1203 <hanaindec@gmail.com>
2022-09-26 14:50:03 +0900

    feat: 마이페이지 프로필 수정 모달

---
+++ client/src/components/layout/Modal/ProfileChangeModalStyle.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ProfileWrapper = styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > h2 {
+    padding-bottom: 16px;
+  }
+  > div.cancel_done_button_wrapper {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    padding-top: 16px;
+  }
+`;
+export const ProfilePhoto = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+export const ProfileName = styled.div`
+  padding: 16px 0;
+`;
