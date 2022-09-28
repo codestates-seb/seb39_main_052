a0c8401d5c64ce8e16e93a50544b9257b098c0c7
hana1203 <hanaindec@gmail.com>
2022-09-28 20:11:49 +0900

    design: 메뉴탭 css 디자인 변경, 마이페이지 컴포넌트 flex로 가운데 위치

--- client/src/components/layout/MenuTab/MenuTab.js
+++ client/src/components/layout/MenuTab/MenuTab.js
@@ -3,15 +3,29 @@ import CommentsBox from "../MyCommentsBox/CommentsBox";
 import MyProfile from "../MyProfile/MyProfile";
 import MyRecipeBox from "../MyRecipeBox/MyRecipeBox";
 import MyRecipeLikedBox from "../MyRecipeLikedBox/MyRecipeLikedBox";
-import { MenuTabContainer, Menu, Li, MenuContent } from "./MenuTabStyle";
+import {
+  MenuTabContainer,
+  Menu,
+  Li,
+  MenuTitle,
+  MenuContent,
+} from "./MenuTabStyle";
 
 const MenuTab = () => {
   const [currentTab, setCurrentTab] = useState(0);
   const menuArr = [
-    { name: "내 프로필", content: <MyProfile /> },
-    { name: "내 레시피", content: <MyRecipeBox /> },
-    { name: "내 좋아요", content: <MyRecipeLikedBox /> },
-    { name: "내 받은 댓글", content: <CommentsBox /> },
+    { name: "내 프로필", title: "", content: <MyProfile /> },
+    {
+      name: "내 레시피",
+      title: "내가 작성한 레시피",
+      content: <MyRecipeBox />,
+    },
+    {
+      name: "내 좋아요",
+      title: "내가 좋아한 레시피",
+      content: <MyRecipeLikedBox />,
+    },
+    { name: "내 받은 댓글", title: "내가 받은 댓글", content: <CommentsBox /> },
   ];
   // console.log(menuArr);
 
@@ -21,7 +35,7 @@ const MenuTab = () => {
 
   return (
     <MenuTabContainer>
-      <Menu>
+      <Menu className="menu">
         {menuArr.map((menu, idx) => (
           <Li
             key={idx}
@@ -32,6 +46,7 @@ const MenuTab = () => {
           </Li>
         ))}
       </Menu>
+      <MenuTitle>{menuArr[currentTab].title}</MenuTitle>
       <MenuContent className="MenuContent">
         {menuArr[currentTab].content}
       </MenuContent>
