c6560692790b8b18ef62864c941b4d0cd600bf5a
imsujinpark <imsujinpark@gmail.com>
2022-09-30 17:12:43 +0900

    chore: 레시피 작성 재료 순서 갯수 제한 변경, 유저 프로필 아이콘 냉장고 이미지로 변경

--- client/src/components/common/UserName/UserName.js
+++ client/src/components/common/UserName/UserName.js
@@ -1,11 +1,11 @@
 import { Image, Name, Wrapper } from "./UserNameStyle";
-import blankImage from "../../../assets/blankImage.webp";
+import logoface from "../../../assets/small_logoface.png";
 
 const UserName = ({ image, name, className }) => {
 
     return (
         <Wrapper className={className}>
-            <Image src={image ? image : blankImage} alt={`profile`} />
+            <Image src={image ? image : logoface} alt={`profile`} />
             <Name>{name}</Name>
         </Wrapper>
     )
