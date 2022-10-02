8eac769cf4a0a7afe034dac0ac8acd8162f4f264
hana1203 <hanaindec@gmail.com>
2022-10-02 11:05:20 +0900

    fix: 시간 변환 함수 초 단위는 방금으로 변경

--- client/src/components/layout/MenuTab/MenuTab.js
+++ client/src/components/layout/MenuTab/MenuTab.js
@@ -12,21 +12,55 @@ import {
   MenuContent,
 } from "./MenuTabStyle";
 
+// !! useParams 냉장고파먹기에서 상세페이지로 갈떄 처럼
 const MenuTab = () => {
+  //실시간 시간 변환
+  function timeSince(date) {
+    let seconds = Math.floor((new Date() - date) / 1000);
+
+    let interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + "년 전";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + "달 전";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + "일 전";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + "시간 전";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + "분 전";
+    }
+    // return Math.floor(seconds) + "초 전";
+    return "방금";
+  }
+
   const [currentTab, setCurrentTab] = useState(0);
   const menuArr = [
     { name: "내 프로필", title: "", content: <MyProfile /> },
     {
       name: "내 레시피",
       title: "내가 작성한 레시피",
-      content: <MyRecipeBox />,
+      content: <MyRecipeBox timeSince={timeSince} />,
     },
     {
       name: "내 좋아요",
       title: "내가 좋아한 레시피",
-      content: <MyRecipeLikedBox />,
+      content: <MyRecipeLikedBox timeSince={timeSince} />,
     },
-    { name: "내 받은 댓글", title: "내가 받은 댓글", content: <CommentsBox /> },
+    {
+      name: "내 받은 댓글",
+      title: "내가 받은 댓글",
+      content: <CommentsBox timeSince={timeSince} />, //시간 변환 함수 내려주기
+    },
   ];
   // console.log(menuArr);
 
