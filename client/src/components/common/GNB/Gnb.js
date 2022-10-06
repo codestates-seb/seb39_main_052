c7967429183891a46c279c3f08da942ea1616e45
hana1203 <hanaindec@gmail.com>
2022-10-06 10:20:01 +0900

    fix: 모달창 닫고 켤때는 searchParams 초기화

--- client/src/components/common/GNB/Gnb.js
+++ client/src/components/common/GNB/Gnb.js
@@ -1,6 +1,6 @@
 import LogOut from "../../layout/DropDown/LogOut";
 import largelogoblack from "../../../assets/largelogoblack.png";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import {
   Header,
   Nav,
@@ -23,11 +23,13 @@ import DropDownMenu from "../../layout/DropDown/DropDownMenu";
 //로그인 상태 가져오려고 리덕스 저장소 정보 import
 import { useSelector, useDispatch } from "react-redux";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RecipeSearchModal from "../../layout/Modal/RecipeSearchModal"; //모달 컴포넌트 만들기 이전 레시피 서치 모달
 import SearchModal from "../../layout/Modal/SearchModal"; //컴포넌트로 만든 이후의 서치 모달
 
 const Gnb = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   //로그인 상태 가져와서 변수에 저장
   const isLoggedIn = useSelector((state) => {
     return state.user.isLoggedIn;
@@ -50,6 +52,15 @@ const Gnb = () => {
 
   //모바일 화면에서 보이는 왼쪽 상단 햄버거 메뉴 토글 상태
   const [isToggleOpen, setIsToggleOpen] = useState(false);
+
+  //모달창 열때 서치파람 없애기
+  useEffect(() => {
+    if (showModal) {
+      console.log("모달창 유즈이펙트");
+      // searchParams.set("keyword", "");
+      setSearchParams({ keyword: "" });
+    }
+  }, [showModal]);
 
   const clickModal = () => {
     setShowModal(!showModal);
