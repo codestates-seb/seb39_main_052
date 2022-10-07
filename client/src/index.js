7b4b8bd4b9d9e198dee739d01fb202da34353cf3
imsujinpark <imsujinpark@gmail.com>
2022-10-07 15:15:04 +0900

    chore: index js console.log 막기

--- client/src/index.js
+++ client/src/index.js
@@ -20,6 +20,9 @@ export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// disable all console.log
+console.log = function() {}
+
 root.render(
   <React.StrictMode>
       <Provider store={store}>
